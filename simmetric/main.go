f0ec666e8ea2a2d71971729dd0bb9e644a898928
Naum Yashuvaev <hahalol915@gmail.com>
2021-04-21 18:01:39 +0300

    done

--- simmetric/main.go
+++ simmetric/main.go
@@ -13,35 +13,29 @@ func main() {
 	if args, err = algo.ParseFlags(); err != nil {
 		flag.Usage()
 	}
-	fmt.Println(args)
-	data := make ([]algo.Visitor, args.NumBilets)
+
+	data := make([]algo.Visitor, args.NumBilets)
 
 	names, err := algo.ReadFile(args.FileName)
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	for i := 0; i < args.NumBilets; i++ { // Initialization of structure
 		data[i].Name = names[i]
-		data[i].Bilet = i+1
+		data[i].Bilet = i + 1
 	}
 
 	// XOR-randomizer
-	for j := 0; j < 1001; j++ {                         // Swap the tickets
-		for i := 0; i < args.NumBilets -1 ; i++ {
-			algo.Swap(&data[(args.Parameter^i+12&j)% args.NumBilets].Bilet,
-			&data[(args.Parameter^j+12&i) % args.NumBilets].Bilet)
+	for j := 0; j < 1001; j++ { // Swap the tickets
+		for i := 0; i < args.NumBilets-1; i++ {
+			algo.Swap(&data[(args.Parameter^i+12&j)%args.NumBilets].Bilet,
+				&data[(args.Parameter^j+12&i)%args.NumBilets].Bilet)
 
 		}
 	}
-	for i := range data {
-		fmt.Println(data[i].Bilet)
-	}
-
 
 	for _, visitor := range data {
 		fmt.Printf("%s: %d\n", visitor.Name, visitor.Bilet)
 	}
-	//fmt.Println(10^2)
 }
-
